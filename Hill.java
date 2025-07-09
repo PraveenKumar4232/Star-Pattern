6bc2dcd42537482f4e463d66aa4ef016b380bc48
Praveen <praveenapk18119@gmail.com>
2025-07-09 20:40:11 +0530

    Updated file

--- Hill.java
+++ Hill.java
@@ -1,58 +1,59 @@
 package starPattern;
-
 public class Hill {
 
 	public static void doubleHill() {
-		for(int i=1; i<=5; i++) {
-			for(int s=1; s<=5-i; s++) {
+		for (int i = 1; i <= 5; i++) {
+			for (int s = 1; s <= 5 - i; s++) {
 				System.out.print(" ");
 			}
-			for(int j=1; j<=i; j++) {
+			for (int j = 1; j <= i; j++) {
+//				System.out.print("* ");
 				System.out.print("* ");
 			}
-			
-			for(int s=1; s<=5-i; s++) {
+
+			for (int s = 1; s <= 5 - i; s++) {
 				System.out.print("  ");
 			}
-			for(int j=1; j<=i; j++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public static void leftDoubleHill() {
-		for(int i=1; i<=5; i++) {
-            for(int j=1; j<=i; j++) {
-                System.out.print("* ");
-            }
-            for(int j=1; j<=5-i; j++) {
-                System.out.print("  ");
-            } 
-            for(int j=1; j<=i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <= 5 - i; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
-	
+
 	public static void rightDoubleHill() {
-		for(int i=1; i<=5; i++) {
-            for(int sp=1; sp<=5-i; sp++) {
-                System.out.print("  ");
-            }
-            for(int j=1; j<=i; j++) {
-                System.out.print("* ");
-            }
-            for(int sp=1; sp<=5-i; sp++) {
-                System.out.print("  ");
-            }
-            for(int j=1; j<=i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+		for (int i = 1; i <= 5; i++) {
+			for (int sp = 1; sp <= 5 - i; sp++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			for (int sp = 1; sp <= 5 - i; sp++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
+
 	public static void main(String[] args) {
 		Hill.doubleHill();
 		System.out.println();
@@ -60,5 +61,4 @@ public class Hill {
 		System.out.println();
 		Hill.rightDoubleHill();
 	}
-
 }
